b070230696c082600dcc859a1bbb5bc01208b13d
SanjarbekO <211amg0707@gmail.com>
2023-08-17 23:16:37 +0600

    add auth

---
+++ controller/auth.js
@@ -0,0 +1,125 @@
+import UsersModel from '../models/users.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+export const registerUser = async (req, res) => {
+    try {
+
+        const {password, ...user} = req.body;
+
+        const idx = await UsersModel.findOne({
+            email: req.body.email
+        });
+
+        if (idx) {
+            res.status(400).json({
+                message: 'Такой аккаунт уже существует'
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc =  new UsersModel({
+            ...user,
+            passwordHash : hash
+        });
+
+        const userCreate = await doc.save();
+
+        const token = jwt.sign({
+            _id: userCreate._id
+        }, 'secret123', {expiresIn: '30d'});
+
+        const {passwordHash, ...userData} = userCreate._doc;
+
+        res.json({
+            user: userData,
+            token
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось зарегистрироваться'
+        })
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try {
+
+        const user = await UsersModel.findOne({email: req.body.email});
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Такого аккаунта не существует'
+            })
+        }
+
+        const inValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!inValidPass) {
+            return res.status(404).json({
+                message: 'Неверный логин или пароль '
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id
+        }, 'secret123' , {expiresIn: '30d'});
+
+        const { passwordHash, ...userData} = user._doc;
+
+        res.json({
+            user: userData,
+            token
+        })
+
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось войти в аккаунт'
+        })
+    }
+};
+
+export const resetPassword = async (req, res) => {
+    try {
+
+        const user = await UsersModel.findOne({email: req.body.email});
+
+        const inValidPass = await bcrypt.compare(req.body.oldPassword, user._doc.passwordHash);
+
+        if (!inValidPass) {
+            return res.status(404).json({
+                message: 'Неверный старый пароль'
+            })
+        }
+
+        if (req.body.oldPassword === req.body.newPassword) {
+            return res.status(404).json({
+                message: 'Старый пароль не должен совпадать с новым'
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.newPassword, salt);
+
+        await UsersModel.updateOne({email: req.body.email}, {
+            passwordHash: hash
+        }, {returnDocument: 'after'});
+
+        res.json({
+            message: 'Пароль изменен',
+            status: 'success'
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось поменять пароль'
+        })
+    }
+};
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express"
 import mongoose from "mongoose";
+import {registerUserValidation,loginUserValidation,resetPasswordValidation} from "./validations/validations.js";
+import handleValidators from "./validations/validations.js"
+import {loginUser, registerUser, resetPassword} from "./controller/auth.js";
+import checkAuth from "./validations/checkAuth.js";
 
 const api = express();
+
+api.use(express.json());
+
+
 
 const mongoDbPassword= '123cdexswzaq123';
 
@@ -10,6 +18,14 @@ mongoose.connect(`mongodb+srv://sanjar:${mongoDbPassword}@sanbay.m21vgpa.mongodb
     .catch((err) => console.log('Ошибка при запуске Mongo DB', err));
 
 const PORT = process.env.PORT || 4444;
+
+// <auth>
+
+api.post('/register', registerUserValidation,handleValidators,registerUser);
+api.post('/login',loginUserValidation,handleValidators,loginUser);
+api.patch('/reset/password',resetPasswordValidation,handleValidators,checkAuth,resetPassword);
+
+// <auth/>
 
 api.listen(PORT,()=>{
     console.log(`Сервер запущен на порту http://localhost:${PORT}`)
