5f3a64e2085fd13f4ded090be9e59db92cecd17f
SanjarbekO <211amg0707@gmail.com>
2023-08-17 21:57:54 +0600

    add validations

---
+++ validations/validations.js
@@ -0,0 +1,40 @@
+import {body} from "express-validator";
+import {validationResult} from "express-validator";
+
+export  default (req,res,next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json(errors.array())
+    }
+    next()
+}
+
+export const registerUserValidation = [
+    body('email','Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен быть минимум 8 символов').isLength({min:8}),
+    body('login', 'Укажите ваш логин').isString(),
+    body('name', 'Укажите ваше имя').isString(),
+    body('number', 'Укажите ваш номер').isNumeric(),
+    body('image', 'Неверный путь').optional().isString()
+];
+
+export const loginUserValidation = [
+    body('email','Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен быть минимум 8 символов').isLength({min:8})
+];
+export const resetPasswordValidation = [
+    body('email','Неверный формат почты').isEmail(),
+    body('oldPassword', 'Пароль должен быть минимум 8 символов').isLength({min:8}),
+    body('newPassword', 'Пароль должен быть минимум 8 символов').isLength({min:8})
+];
+
+
+export const addOrderValidation = [
+    body('title', 'Заголовок объявления минимум 2 символа').isLength({min:2}),
+    body('description', 'Описание объявления минимум 3 символа').isLength({min:3}),
+    body('category', 'Неверный формат категории').isString(),
+    body('price', 'Неверный формат цены').isNumeric(),
+    body('views', 'Неверный формат просмотров').isNumeric(),
+    body('status', 'Неверный формат статуса').isString(),
+    body('creatorId','Неверный формат id создателя').isMongoId()
+];
